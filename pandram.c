a6954032dcb57bb2091dc63f47e55a79312f0377
192211922 <124116787+192211922@users.noreply.github.com>
2023-02-27 23:50:17 -0800

    Create pandram.c

---
+++ pandram.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char str[100];
+    int i;
+    int upper=0,lower=0,num=0,special=0;;
+    printf("Please enter the string \n");
+    gets(str); 
+    i=0;
+    while(str[i] != '\0'){
+            if(str[i]>='A' && str[i]<='Z') {
+                upper++;
+    }else if(str[i]>='a' && str[i]<='z') {
+                lower++;
+    }else if(str[i]>='1' && str[i]<='9') {
+                num++;
+    }
+    else{
+        special++;
+    }
+    i++;
+    }
+    printf("\nUpper case letters: %d",upper);
+    printf("\nLower case letters: %d",lower);
+    printf("\nnumbers: %d",num);
+    printf("\nSpecial characters: %d",special);
+    return 0;
+}
